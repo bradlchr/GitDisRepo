3b1b3327623944fad4d7e9a150cbeacc87682032
Adrian Eves <adrianevesdev@gmail.com>
2020-10-28 13:37:05 -0500

    Initial commit for my weird test view

---
+++ Shared/AdrianView.swift
@@ -0,0 +1,20 @@
+//
+//  AdrianView.swift
+//  GitDisRepo
+//
+//  Created by Adrian Eves on 10/28/20.
+//
+
+import SwiftUI
+
+struct AdrianView: View {
+    var body: some View {
+        Text("This is a view I'm pushing to my branch!")
+    }
+}
+
+struct AdrianView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdrianView()
+    }
+}
