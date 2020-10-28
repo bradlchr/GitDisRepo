919a87bf94cc472f302b35bee8aac0dad2a1d971
Christopher Bradley <bradlchr@icloud.com>
2020-10-28 12:38:40 -0500

    Initial commit.

---
+++ Shared/GitDisRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDisRepoApp.swift
+//  Shared
+//
+//  Created by Christopher Bradley on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct GitDisRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
