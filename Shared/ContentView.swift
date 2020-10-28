cd2e8b57d8ad8c720c1632fbba15df22ef4d5dcc
Christopher Bradley <bradlchr@icloud.com>
2020-10-28 14:05:24 -0500

    NavigationTitle

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List (0..<5) {
-            Text("This is list item \($0)")
+        NavigationView {
+            List (0..<5) {
+                Text("This is list item \($0)")
+            }
+            .navigationTitle("Chris' Branch")
         }
     }
 }
